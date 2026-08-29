98a1b17fc4e43d2238d981d6846ae38c8743a6e4
jenaro fv <jen4r0@gmail.com>
2026-08-29 16:21:19 -0600

    add instructions

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ function App() {
         <h1>Memory Card</h1>
       </header>
       <hr />
+      <div className="instructions">
+        <h2> Instructions </h2>
+        <p> Click all the pictures, without clicking any twice.</p>
+      </div>
     </>
   );
 }
