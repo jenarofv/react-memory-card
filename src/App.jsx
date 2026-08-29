d43e86ed820aed0f6ad296b662f714626af2b92c
jenaro fv <jen4r0@gmail.com>
2026-08-29 16:15:23 -0600

    --amend

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>Memory Card</h1>
+      </header>
+      <hr />
+    </>
+  );
 }
 
 export default App;
