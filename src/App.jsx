72e58aa8b9474ef7e1efb7e8dbba4bcdc94c2b4d
jenaro fv <jen4r0@gmail.com>
2026-08-30 09:29:32 -0600

    display pug pictures from the Dog.ceo API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+const apiUrl = "https://dog.ceo/api/breed/pug/images";
 
 function App() {
+  const [currScore, setCurrScore] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const request = new Request(apiUrl);
+    fetch(apiUrl, { mode: "cors" })
+      .then((result) => result.json())
+      .then((json) => {
+        const imagesAux = [];
+        for (let i = 0; i < 16; i++) {
+          imagesAux.push({ key: i, src: json.message[i], clicked: false });
+        }
+        console.log(imagesAux);
+        setImages(imagesAux);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <header>
@@ -11,6 +31,11 @@ function App() {
       <div className="instructions">
         <h2> Instructions </h2>
         <p> Click all the pictures, without clicking any twice.</p>
+      </div>
+      <div className="images">
+        {images.map((image) => (
+          <img key={image.key} className="card" src={image.src} />
+        ))}
       </div>
     </>
   );
